ec467dc58adf64492cf0953ec27057767fb58085
HeXudong <1514133606@qq.com>
2023-02-08 15:54:55 +0800

    exe10.6~10.7

--- rate-repository-app/src/components/AppBar.jsx
+++ rate-repository-app/src/components/AppBar.jsx
@@ -1,27 +1,50 @@
-import { View, Text, StyleSheet, Pressable } from 'react-native';
-import Constants from 'expo-constants';
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+import Constants from "expo-constants";
+import { Link } from "react-router-native";
 
 const styles = StyleSheet.create({
-    container: {
-        paddingTop: Constants.statusBarHeight,
-        backgroundColor: '#24292e',
-    },
-    text: {
-        color: '#fff',
-        lineHeight:  50,
-        fontSize: 16,
-        fontWeight: 'bold'
-    }
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: "#24292e",
+  },
+  link: {
+    marginRight: 20,
+  },
+  text: {
+    color: "#fff",
+    lineHeight: 50,
+    fontSize: 16,
+    fontWeight: "bold",
+  },
 });
 
 const AppBar = () => {
-    return (
-        <View style={styles.container}>
-            <Pressable>
-                <Text style={styles.text}>Repositories</Text>
-            </Pressable>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <ScrollView horizontal>
+        <Link to="/" style={styles.link}>
+          <Text style={styles.text}>Repositories</Text>
+        </Link>
+        <Link to="/signin" style={styles.link}>
+          <Text style={styles.text}>Login</Text>
+        </Link>
+        <Link to="/signin" style={styles.link}>
+          <Text style={styles.text}>Login</Text>
+        </Link>
+        <Link to="/signin" style={styles.link}>
+          <Text style={styles.text}>Login</Text>
+        </Link>
+        <Link to="/signin" style={styles.link}>
+          <Text style={styles.text}>Login</Text>
+        </Link>
+        <Link to="/signin" style={styles.link}>
+          <Text style={styles.text}>Login</Text>
+        </Link>
+      </ScrollView>
+    </View>
+  );
 };
 
 export default AppBar;
