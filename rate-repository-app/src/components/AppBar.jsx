3261c3dcf07027e1faf19c1e64c321981c0fdb76
HeXudong <1514133606@qq.com>
2023-02-08 22:38:11 +0800

    exe10.15~10.16

--- rate-repository-app/src/components/AppBar.jsx
+++ rate-repository-app/src/components/AppBar.jsx
@@ -1,6 +1,10 @@
-import { View, Text, StyleSheet, ScrollView } from "react-native";
+import { View, Text, StyleSheet, ScrollView, Pressable } from "react-native";
+import { useQuery, useApolloClient } from '@apollo/client';
 import Constants from "expo-constants";
 import { Link } from "react-router-native";
+
+import useAuthStorage from '../hooks/useAuthStorage';
+import { GET_ME } from "../graphql/queries";
 
 const styles = StyleSheet.create({
   container: {
@@ -21,15 +25,46 @@ const styles = StyleSheet.create({
 });
 
 const AppBar = () => {
+  const authStorage = useAuthStorage();
+  const apolloClient = useApolloClient()
+
+  const { data, loading } = useQuery(GET_ME, {
+    fetchPolicy: 'cache-and-network',
+  });
+
+  const onLogout = () => {
+    authStorage.removeAccessToken()
+    apolloClient.resetStore()
+  }
+
+  if (loading) {
+    return (
+      <View>
+        <Text>loading...</Text>
+      </View>
+    )
+  }
+
+  console.log('data', data)
+
   return (
     <View style={styles.container}>
       <ScrollView horizontal>
         <Link to="/" style={styles.link}>
           <Text style={styles.text}>Repositories</Text>
         </Link>
-        <Link to="/signin" style={styles.link}>
-          <Text style={styles.text}>Login</Text>
-        </Link>
+        {
+          !data.me &&
+          <Link to="/signin" style={styles.link}>
+            <Text style={styles.text}>Login</Text>
+          </Link>
+        }
+        {
+          data.me &&
+          <Pressable onPress={onLogout}>
+            <Text style={styles.text}>Logout</Text>
+          </Pressable>
+        }
       </ScrollView>
     </View>
   );
--- rate-repository-app/src/components/SignIn.jsx
+++ rate-repository-app/src/components/SignIn.jsx
@@ -1,10 +1,14 @@
 import { Pressable, View, StyleSheet } from "react-native";
+import { useNavigate } from "react-router-native";
+import { useApolloClient } from '@apollo/client';
 import { Formik } from "formik";
 import { string, object } from 'yup';
 import Text from "./Text";
 import FormikTextInput from "./FormikTextInput";
 import theme from "../theme";
 import useSignIn from "../hooks/useSignIn";
+import useAuthStorage from '../hooks/useAuthStorage';
+
 
 const validationSchema = object().shape({
   username:
@@ -53,12 +57,17 @@ const BodyMassIndexForm = ({ onSubmit }) => {
 
 const SignIn = () => {
   const [signIn] = useSignIn();
+  const authStorage = useAuthStorage();
+  const apolloClient = useApolloClient()
+  const navigate = useNavigate();
 
   const onSubmit = async (values) => {
     const { username, password } = values;
-
     try {
       const { data } = await signIn({ username, password });
+      authStorage.setAccessToken(data.authenticate.accessToken)
+      apolloClient.resetStore();
+      navigate('/')
       console.log(data);
     } catch (e) {
       console.log(e);
