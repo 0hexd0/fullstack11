d64eb23d53b984aded2545298c4cdf1fc1b78a41
HeXudong <1514133606@qq.com>
2023-02-08 11:08:08 +0800

    add styles

--- rate-repository-app/src/components/RepositoryItem.jsx
+++ rate-repository-app/src/components/RepositoryItem.jsx
@@ -1,56 +1,105 @@
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { View, Text, StyleSheet, Image } from "react-native";
 
-const headLineHeight = 30
+const headLineHeight = 30;
+
+export const padding = (a, b, c, d) => ({
+    paddingTop: a,
+    paddingRight: b ?? a,
+    paddingBottom: c ?? a,
+    paddingLeft: d ?? b ?? a,
+});
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: 'white'
+        backgroundColor: "white",
+        padding: 20,
     },
     header: {
-        display: 'flex',
-        flexDirection: 'row',
-        padding: 20
+        display: "flex",
+        flexDirection: "row",
     },
     fullName: {
-        fontWeight: 'bold',
-        lineHeight: headLineHeight
+        fontWeight: "bold",
+        lineHeight: headLineHeight,
     },
     language: {
         color: "white",
-        backgroundColor: '#0366d6'
+        backgroundColor: "#0366d6",
+        lineHeight: headLineHeight,
+        borderColor: "red",
+        alignSelf: "flex-start",
+        padding: 5,
+        borderRadius: 4,
     },
     description: {
-        lineHeight: headLineHeight
+        marginTop: 6,
+        marginBottom: 10
     },
     profile: {
         width: 50,
         height: 50,
-        marginRight: 20
+        marginRight: 20,
+    },
+    infos: {
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-around",
+        marginTop: 20,
+    },
+    info: {
+        display: "flex",
+        alignItems: "center",
+    },
+    count: {
+        fontWeight: "bold",
+        marginBottom: 10,
+    },
+    whiteText: {
+        color: "white",
     },
 });
 
 const RepositoryItem = (props) => {
-    const repository = props.repository
+    const repository = props.repository;
     return (
         <View style={styles.container}>
             <View style={styles.header}>
-                <Image style={styles.profile} source={{
-                    uri: repository.ownerAvatarUrl
-                }} />
-                <View>
+                <Image
+                    style={styles.profile}
+                    source={{
+                        uri: repository.ownerAvatarUrl,
+                    }}
+                />
+                <View style={{ flexShrink: 1 }}>
                     <Text style={styles.fullName}>{repository.fullName}</Text>
                     <Text style={styles.description}>{repository.description}</Text>
-                    <Text style={styles.language}> {repository.language}</Text>
+                    <View style={styles.language}>
+                        <Text style={styles.whiteText}>{repository.language}</Text>
+                    </View>
                 </View>
             </View>
 
-
-            <Text>Stars: {repository.stargazersCount}</Text>
-            <Text>Forks: {repository.forksCount}</Text>
-            <Text>Reviews: {repository.reviewCount}</Text>
-            <Text>Rating: {repository.ratingAverage}</Text>
+            <View style={styles.infos}>
+                <View style={styles.info}>
+                    <Text style={styles.count}>{repository.stargazersCount}</Text>
+                    <Text>Stars</Text>
+                </View>
+                <View style={styles.info}>
+                    <Text style={styles.count}>{repository.forksCount}</Text>
+                    <Text>Forks</Text>
+                </View>
+                <View style={styles.info}>
+                    <Text style={styles.count}>{repository.reviewCount}</Text>
+                    <Text>Reviews</Text>
+                </View>
+                <View style={styles.info}>
+                    <Text style={styles.count}>{repository.ratingAverage}</Text>
+                    <Text>Rating</Text>
+                </View>
+            </View>
         </View>
-    )
-}
+    );
+};
 
-export default RepositoryItem
+export default RepositoryItem;
